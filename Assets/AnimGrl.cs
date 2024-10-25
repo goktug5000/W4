aac89f87fcf0cffccf84fa66c7dde1ae6ba964a3
Rahman2001 <rahmanrejepov777@gmail.com>
2024-10-25 21:13:19 +0300

    Anime girl and movements

---
+++ Assets/AnimGrl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimGrl : MonoBehaviour
+{
+    public GameObject WalkinSFX;
+
+    void Start()
+    {
+        WalkinSFX.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)
+            || Input.GetKey(KeyCode.W)
+            || Input.GetKey(KeyCode.S)
+            || Input.GetKey(KeyCode.D))
+        {
+            WalkinSFX.SetActive(true);
+        }
+        else
+        {
+            WalkinSFX.SetActive(false);
+        }
+    }
+}
